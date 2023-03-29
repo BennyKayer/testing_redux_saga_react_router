0333495eed36e24200c57a77c9f37bcfe0e13c32
Paweł Benkowski <pawlo97.pb@gmail.com>
2023-03-29 20:01:19 +0200

    #03.15 Error from not returning a saga

---
+++ src/features/tickets/redux/ticketSaga.test.ts
@@ -0,0 +1,59 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import axios, { CancelTokenSource } from "axios";
+import { SagaIterator } from "redux-saga";
+import {
+  call,
+  cancel,
+  cancelled,
+  put,
+  race,
+  select,
+  take,
+  takeEvery,
+} from "redux-saga/effects";
+import { expectSaga } from "redux-saga-test-plan";
+
+import { HoldReservation } from "../../../../../shared/types";
+import {
+  holdReservation,
+  purchasePayload,
+  purchaseReservation,
+} from "../../../test-utils/fake-data";
+import { showToast } from "../../toast/redux/toastSlice";
+import { ToastOptions } from "../../toast/types";
+import {
+  cancelPurchaseServerCall,
+  releaseServerCall,
+  reserveTicketServerCall,
+} from "../api";
+import { TicketAction } from "../types";
+import {
+  cancelTransaction,
+  generateErrorToastOptions,
+  purchaseTickets,
+  ticketFlow,
+} from "./ticketSaga";
+import {
+  endTransaction,
+  holdTickets,
+  PurchasePayload,
+  ReleasePayload,
+  resetTransaction,
+  selectors,
+  startTicketAbort,
+  startTicketPurchase,
+  startTicketRelease,
+} from "./ticketSlice";
+
+const holdAction = {
+  type: "test",
+  payload: holdReservation,
+};
+
+describe("common to all flows", () => {
+  test("starts with hold call to server", () => {
+    return expectSaga(ticketFlow, holdAction)
+      .call(reserveTicketServerCall, holdAction.payload)
+      .run();
+  });
+});
